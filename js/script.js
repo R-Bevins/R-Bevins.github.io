d5a47e4f1944ab2f309ed60cc53fbb122fc49316
R-Bevins <42890575+R-Bevins@users.noreply.github.com>
2025-10-19 20:04:50 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,155 @@
+function changeImage(a) {
+    document.getElementById("img").src=a;
+    document.getElementById("mainImageDisplay").style.display="block";
+    document.getElementById("mainVideoDisplay").style.display="none";
+    document.getElementById("video").src=a;
+    document.getElementById("lbImage").href=a;
+    
+        }
+function changeVideo(a) {
+    document.getElementById("video").src=a;
+    document.getElementById("mainVideoDisplay").style.display="block";
+    document.getElementById("mainImageDisplay").style.display="none";
+        }
+function changeAlt(a) {
+    document.getElementById("img").alt=a;
+}
+
+function changeTextCity(){
+document.getElementById("outputD").innerHTML = "This piece is a 3-point perspective city I made. It was a drawing assignment in my Computer Animation and Web Design class. It was the final assignment in our mini unit about perspective. In the previous weeks, my class had learned about 1-point and 2-point perspective, and I was very excited to learn about 3-point perspective. I ended up really liking the piece I made. I thought the use of 3-point perspective helped to make the buildings look interesting and like they're towering high up into the sky as the viewer looks down on them. It was drawn traditionally using pencils on January 16th, 2022.";
+document.getElementById("outputD2").innerHTML = "";
+document.getElementById("outputD3").innerHTML = "";
+document.getElementById("outputD4").innerHTML = "";
+document.getElementById("outputD5").innerHTML = "";
+document.getElementById("outputN").innerHTML ="3-Point Perspective City";
+document.getElementById("lbImage").title= "3-Point Perspective Buildings";
+}
+function changeTextDragon(){
+    document.getElementById("outputD").innerHTML = "Dragon Kidnapping was a one-page comic I made in order to work on improving my paneling and composition skills for storytelling. I tried to make it so the panels flow well together in the proper order and in a way that enhances the short story being told. I was trying to make it so there was a lot of build up to make the subversion at the end hit properly. It was drawn on September 12th, 2022";
+    document.getElementById("outputD2").innerHTML = "";
+document.getElementById("outputD3").innerHTML = "";
+document.getElementById("outputD4").innerHTML = "";
+document.getElementById("outputD5").innerHTML = "";
+    document.getElementById("outputN").innerHTML ="Dragon Kidnapping";
+    document.getElementById("lbImage").title= "Dragon Kidnapping";
+    document.getElementsByClassName("lb-caption").innerHTML= "Dragon Kidnapping";
+    }
+
+function changeTextMagic(){
+    document.getElementById("outputD").innerHTML = "This is a comic I drew for Graphic Novel Production class midterm. I think it came out quite well, and I think I managed to handle the panelling with clear intention. I do wish I could've done more with the shading and backgrounds though. It was completed on October 10th, 2025, and made in Krita.";
+        document.getElementById("outputD2").innerHTML = "<a href=https://drive.google.com/file/d/1ANQtYQmlc8AINYMKA3zzN36SogrRqu7w/view?usp=sharing>Comic Link</a>";
+    document.getElementById("outputD3").innerHTML = "";
+    document.getElementById("outputD4").innerHTML = "";
+    document.getElementById("outputD5").innerHTML = "";
+        document.getElementById("outputN").innerHTML ="Magician's Secret";
+        document.getElementById("lbImage").title= "Magician's Secret";
+        document.getElementsByClassName("lb-caption").innerHTML= "Magician's Secret";
+        }
+function changeTextRyan(){
+    document.getElementById("outputD").innerHTML = "This is a self portrait I drew. It was drawn with pencil and completed on December 23rd, 2022.";
+        document.getElementById("outputD2").innerHTML = "";
+    document.getElementById("outputD3").innerHTML = "";
+    document.getElementById("outputD4").innerHTML = "";
+    document.getElementById("outputD5").innerHTML = "";
+        document.getElementById("outputN").innerHTML ="Self Portrait";
+        document.getElementById("lbImage").title= "Self Portrait";
+        document.getElementsByClassName("lb-caption").innerHTML= "SelfPortrait";
+                }
+function changeTextBottles(){
+    document.getElementById("outputD").innerHTML = "This is a drawing of bottles I did for my Computer Animation and Web Design Class. It was an exercise meant to practice working with perspective on organic shapes.";
+        document.getElementById("outputD2").innerHTML = "It was completed in pencil on February 8th, 2022.";
+    document.getElementById("outputD3").innerHTML = "";
+    document.getElementById("outputD5").innerHTML = "";
+    document.getElementById("outputN").innerHTML ="Bottles";
+    document.getElementById("lbImage").title= "Bottles";
+    document.getElementsByClassName("lb-caption").innerHTML= "Bottles";
+                                }
+function changeTextFantasy(){
+    document.getElementById("outputD").innerHTML = "This is a 1-point perspective drawing I made of a generic fantasy town I designed.";
+    document.getElementById("outputD2").innerHTML = "It was drawn in pencil and completed on November 9th, 2021.";
+    document.getElementById("outputD3").innerHTML = "";
+    document.getElementById("outputD5").innerHTML = "";
+    document.getElementById("outputN").innerHTML ="1-Point Perspective Fantasy Town";
+        }
+function changeTextClock(){
+document.getElementById("outputD").innerHTML = "This piece was made as part of an assignment for my 2D Design 1 class where we had to take complex objects, disassemble them, and then make a new drawing inspired by those pieces. I took an analogue clock and a digital clock, pulled them apart, and then drew this based on the pieces from both of them.";
+document.getElementById("outputD2").innerHTML = "It was drawn in Krita and completed on October 30th, 2023.";
+document.getElementById("outputD3").innerHTML = "";
+document.getElementById("outputD4").innerHTML = "";
+document.getElementById("outputD5").innerHTML = "";
+document.getElementById("outputN").innerHTML ="Clock Drawing";
+}
+
+function changeTextMWD(){
+    document.getElementById("outputD").innerHTML = "This is one drawing from a series of Mother Ducking Witch pieces I've made. They were originally inspired by inside jokes from some of my friends relating to how I avoid swearing. I played into those jokes by continuing to censor myself in even wackier and comedic ways. One of those censors was Mother Ducking Witch, and I realized that it could be a fun character to draw. Which is what made me first draw a piece about a witch duck and her duckling. This specific piece was my 5th Mother Ducking Witch drawing, and my personal favorite. The drawing was made in Medibang on July 16th, 2021.";
+    document.getElementById("outputD2").innerHTML = "";
+document.getElementById("outputD3").innerHTML = "";
+document.getElementById("outputD4").innerHTML = "";
+document.getElementById("outputD5").innerHTML = "";
+    document.getElementById("outputN").innerHTML ="Mother Ducking Witch";
+    }
+
+    function changeTextEthic(){
+        document.getElementById("outputD").innerHTML = "A pencil drawn comic made for my Ethics in the Graphic Memoir class. The assignment was to draw a 9 panel comic based on a moment from your own life, so I decided to get meta about it and draw me drawing the comic. ";
+        document.getElementById("outputD2").innerHTML = "It was completed January 19th, 2025.";
+document.getElementById("outputD3").innerHTML = "";
+document.getElementById("outputD4").innerHTML = "";
+document.getElementById("outputD5").innerHTML = "";
+        document.getElementById("outputN").innerHTML ="Meta Memoir Comic";
+        }
+function changeTextBoxing(){
+    document.getElementById("outputD").innerHTML = "Match of the Century was a comic page I made with a specific goal in mind. I wanted to try and make a comic that was able to tell a story without using a single line of dialogue. I did this since it would force me to focus on the expressions and poses of the characters along with the layout and composition in order to deliver a story. I did use some text for sound effects and the victory announcement at the end, but those are just there to enhance the art in the panels rather than the main method of storytelling. Overall, I think the piece turned out well and definitely helped to improve my storytelling. It was completed on October 30th, 2022, in Krita.";
+    document.getElementById("outputD2").innerHTML = "";
+document.getElementById("outputD3").innerHTML = "";
+document.getElementById("outputD4").innerHTML = "";
+document.getElementById("outputD5").innerHTML = "";
+    document.getElementById("outputN").innerHTML ="Match of the Century";
+    }
+function changeTextSoda(){
+    document.getElementById("outputD5").innerHTML = "";
+    document.getElementById("outputD").innerHTML = "This work sample is a still life drawing I did of Pepsi and Coke bottles fighting. This was a project I did on my own in order to strengthen my college portfolio. It had been recommended to me during portfolio reviews that I should add more still life drawings made from direct observation and that I should have more variety in the media that I work with.  Making this still life piece using colored pencils helped with that. I was mainly inspired by the cases of Pepsi and Coke in my house and I thought a sort of brand war between them would make for a funny piece.";
+document.getElementById("outputD2").innerHTML = "The procedure to make it started with putting together the actual reference. I arranged the various bottles and taped plastic forks and knives where they needed to be. From there, I started lightly sketching out the shapes of all the objects. Once that was done, I colored and shaded all of the objects. It ended up coming out quite well with no major revisions needed.";
+document.getElementById("outputD3").innerHTML = "I really like this piece, it was one I put a lot of effort into. It’s a good example of my ability to work off of direct observation and my ability to work with physical media. It also helped me further develop my skills with colored pencils greatly. Overall, one of my favorite pieces. The only thing I’d really change about it is making the line direction more consistent in order to make some sections (especially the box) look less scribbly.";
+document.getElementById("outputD4").innerHTML = "";
+    document.getElementById("outputN").innerHTML ="Soda War";
+    }
+function changeTextInktober(){
+    document.getElementById("outputD").innerHTML = "I participated in Inktober 2024, but with the twist that I'd draw all of my Inktober pieces in Microsoft Paint, using only the trackpad on my laptop for most of them (there were a couple exceptions where I had to use my phone for it just to meet the deadlines). I also restricted myself to its default grayscale color palette.";
+    document.getElementById("outputD2").innerHTML = "For October 24th, 2024, I drew an astronaut exploring space for the Expedition prompt.";
+document.getElementById("outputD3").innerHTML = "For October 18th, 2024, I drew a rocket car driving through space for the Drive prompt.";
+document.getElementById("outputD4").innerHTML = "For October 8th, 2024, I drew a football about to be hiked for the Hiking prompt. You may noticed I left a watermark for the tumblr account I posted all of these on. That was something I originally intended to put on all of the pieces, but forgot.";
+document.getElementById("outputD5").innerHTML = "For October 16th, 2024, I drew a trashcan for the Grungy prompt.";
+    document.getElementById("outputN").innerHTML ="My Best of Inktober 2024";
+    }
+function changeTextSticky(){
+    document.getElementById("outputD").innerHTML = "I've had a pad of stickynotes on my desk, and whenever I needed a quick break from whatever assignment I was working on, I'd do a quick doodle on one, and these are my favorite 25 so far. Some of these ended up reflecting my emotional state still working at 3:00am, but most of them just ended up being quick character doodles. All of them were completed between September 1st, 2025, and October 17th, 2025, using either pens or pencils.";
+    document.getElementById("outputD2").innerHTML = "";
+document.getElementById("outputD3").innerHTML = "";
+document.getElementById("outputD4").innerHTML = "";
+document.getElementById("outputD5").innerHTML = "";
+    document.getElementById("outputN").innerHTML ="Stickynote Doodles";
+    }
+function changeTextFruit(){
+    document.getElementById("outputD").innerHTML = "A bowl of fruit and a potato drawn from observation in pencil, completed on September 29th, 2022.";
+    document.getElementById("outputD2").innerHTML = "";
+document.getElementById("outputD3").innerHTML = "";
+document.getElementById("outputD4").innerHTML = "";
+document.getElementById("outputD5").innerHTML = "";
+    document.getElementById("outputN").innerHTML ="Bowl of Fruit + Potato";
+    }
+function changeTextCat(){
+    document.getElementById("outputD").innerHTML = "This drawing was made for one of my 2D Design course assignements. We were all given two random strips of paper with prompts on them, and told to run wild from there with how we interpret them.";
+    document.getElementById("outputD2").innerHTML = "My prompts were Fear and Running, so I thought about what one might do if they had a fear of running, and that made me think of cats on roombas, and that eventually brought me here.";
+document.getElementById("outputD3").innerHTML = "It was completed on September 6th, 2023, using alcohol markers.";
+document.getElementById("outputD4").innerHTML = "";
+document.getElementById("outputD5").innerHTML = "";
+    document.getElementById("outputN").innerHTML ="After the 9th Life";
+    }
+function changeTextCorner(){
+    document.getElementById("outputD").innerHTML = "This piece is a 2-point perspective drawing of a city corner I drew. It was drawn with pencil and completed on December 7th, 2021.";
+    document.getElementById("outputD2").innerHTML = "";
+document.getElementById("outputD3").innerHTML = "";
+document.getElementById("outputD4").innerHTML = "";
+document.getElementById("outputD5").innerHTML = "";
+    document.getElementById("outputN").innerHTML ="2-Point Perspective City Corner";
+}
